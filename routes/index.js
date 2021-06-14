ea5c60397606cb13285b7b7cf5a7747b3b9da1b6
wlcharlie <wl00606352@gmail.com>
2021-06-14 14:41:26 +0800

    add index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
+const restController = require('../controllers/restController')
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.redirect('restaurants')
   })
+
+  app.get('/restaurants', restController.getRestaurants)
 }
