ce8feba1212cf510932ad8e81ba52d6d671e841b
wlcharlie <wl00606352@gmail.com>
2021-06-16 10:18:04 +0800

    add create function

--- routes/index.js
+++ routes/index.js
@@ -19,15 +19,18 @@ module.exports = (app, passport) => {
     res.redirect('/signin')
   }
 
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
-
   app.get('/', authenticated, (req, res) => {
     res.redirect('restaurants')
   })
+
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
 
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+
+  app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
+  app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
