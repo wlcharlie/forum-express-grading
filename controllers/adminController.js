106616a7f68d6139a6c61d35398e1ebd3e1547a6
wlcharlie <wl00606352@gmail.com>
2021-06-16 12:11:05 +0800

    add delete function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -65,6 +65,16 @@ const adminController = {
           })
       })
   },
+
+  deleteRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.destroy()
+          .then((restaurant) => {
+            res.redirect('/admin/restaurants')
+          })
+      })
+  }
 }
 
 
