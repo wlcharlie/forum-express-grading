0324ac87e956a7c5f5a6896b8697cd066d378c66
wlcharlie <wl00606352@gmail.com>
2021-06-16 10:20:32 +0800

    add read function (admin)

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -29,6 +29,14 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
   },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+      return res.render('admin/restaurant', {
+        restaurant: restaurant
+      })
+    })
+  }
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,10 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
 
+  app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+
+
+
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
 
