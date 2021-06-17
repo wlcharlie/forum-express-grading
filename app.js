f26aeefc3684cd38b8bea033e4e507b54d2e1e12
wlcharlie <wl00606352@gmail.com>
2021-06-17 10:33:42 +0800

    use imgur API and env setting

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
 const session = require('express-session')
