88f8758cd68710355ad80ae65e7d69649120b891
wlcharlie <wl00606352@gmail.com>
2021-06-21 16:48:34 +0800

    create category model and add restaurant & category association

---
+++ models/category.js
@@ -0,0 +1,19 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+
+    static associate(models) {
+      Category.hasMany(models.Restaurant)
+    }
+  };
+  Category.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+  });
+  return Category;
+};
