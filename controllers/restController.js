84a0c4f2a830c8b99940764827dfef8f6628db4d
wlcharlie <wl00606352@gmail.com>
2021-06-22 19:22:03 +0800

    add paginatior on restaurants page

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,35 +2,52 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
+    const offset = (req.query.page - 1) * pageLimit || 0
+
     const whereQuery = {}
     const categoryId = Number(req.query.categoryId) || ''
+
     if (categoryId) {
       whereQuery.CategoryId = categoryId
     }
 
-    Restaurant.findAll({
+    Restaurant.findAndCountAll({
       include: Category,
       where: whereQuery,
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          categoryName: r.Category.name
-        }))
-        Category.findAll({ raw: true, nest: true })
-          .then(categories => {
-            return res.render('restaurants', {
-              restaurants: data,
-              categories: categories,
-              categoryId
-            })
-          })
+      raw: true, nest: true,
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? false : page - 1
+      const next = page + 1 > pages ? false : page + 1
+
+      const data = result.rows.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50),
+        categoryName: r.Category.name
+      }))
+      Category.findAll({
+        raw: true,
+        nest: true
+      }).then(categories => {
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
+        })
       })
+    })
   },
 
   getRestaurant: (req, res) => {
