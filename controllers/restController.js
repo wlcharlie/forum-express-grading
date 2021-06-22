985bdacbd15c7d12b38c4dad556563ffc06b775c
wlcharlie <wl00606352@gmail.com>
2021-06-22 19:01:35 +0800

    add categories navbar

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,16 +4,32 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category, raw: true, nest: true })
+    const whereQuery = {}
+    const categoryId = Number(req.query.categoryId) || ''
+    if (categoryId) {
+      whereQuery.CategoryId = categoryId
+    }
+
+    Restaurant.findAll({
+      include: Category,
+      where: whereQuery,
+      raw: true,
+      nest: true
+    })
       .then(restaurants => {
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50),
           categoryName: r.Category.name
         }))
-        return res.render('restaurants', {
-          restaurants: data
-        })
+        Category.findAll({ raw: true, nest: true })
+          .then(categories => {
+            return res.render('restaurants', {
+              restaurants: data,
+              categories: categories,
+              categoryId
+            })
+          })
       })
   },
 
