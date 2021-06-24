d1a98b83452d95476f5af80356c458754268a3d6
wlcharlie <wl00606352@gmail.com>
2021-06-24 20:15:35 +0800

    A20_Q2: adjust restaurants.viewCounts deafult to 0

--- seeders/3-20210617012231-restaurants-seed-file.js
+++ seeders/3-20210617012231-restaurants-seed-file.js
@@ -15,7 +15,8 @@ module.exports = {
         description: faker.lorem.text(),
         createdAt: new Date(),
         updatedAt: new Date(),
-        CategoryId: Math.floor(Math.random() * 6) * 10 + 1
+        CategoryId: Math.floor(Math.random() * 6) * 10 + 1,
+        viewCounts: 0
       })
       ), {})
   },
