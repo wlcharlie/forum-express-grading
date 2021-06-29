32fdf85a7cb142b2fc650a6a0835f7ae919d491f
wlcharlie <wl00606352@gmail.com>
2021-06-29 21:46:30 +0800

    add authenticated and authenticatedAdmin

--- routes/apis.js
+++ routes/apis.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('../config/passport')
 
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -9,10 +10,21 @@ const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController')
 const userController = require('../controllers/api/userController.js')
 
+const authenticated = passport.authenticate('jwt', { session: false })
 
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
 router.post('/signin', userController.signIn)
 
-router.get('/admin/restaurants', adminController.getRestaurants)
+// router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
